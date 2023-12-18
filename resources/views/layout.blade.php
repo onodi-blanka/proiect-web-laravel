a85c3ec4e85f4af682c59acc5e78fb19957ac6e5
Emilia-Maria <nuta.emilia01@gmail.com>
2023-12-18 18:00:39 +0200

    Reparat logout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,6 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Include your custom stylesheet -->
 {{--    @stack('css')--}}
 {{--    @push('css')--}}
